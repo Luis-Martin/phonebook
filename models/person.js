77d3fecb3577d53334c64acda0ad5789693cd5c2
Luis Martin <luis.aaron.mp17@gmail.com>
2024-03-24 15:40:39 -0500

    connection with the database moved into the index

--- models/person.js
+++ models/person.js
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose'
 
-mongoose.set('strictQuery', false)
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(res => console.log('Connected to MongoDB'))
-  .catch(err => console.log('error connecting to MongoDB:', err.message))
-
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
